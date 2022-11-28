2b60088e61bf35b47b63ec2e7e2db16e4db488d7
donghun93 <alswn4516@gmail.com>
2022-11-28 23:11:45 +0900

    feat: first init

---
+++ src/main/java/com/example/villagerservice/VillagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.villagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillagerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/villagerservice/VillagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.villagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
