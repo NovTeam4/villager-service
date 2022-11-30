3ce90b6587baeb6ae28e76b0c78b894a06cce472
donghun93 <alswn4516@gmail.com>
2022-11-30 12:26:40 +0900

    #7 - 회원 가입 컨트롤러 개발
    - 스프링 시큐리티에서 /api/v1/auth/** 경로에 대해서는 인증 없이 모두 허가함
    - 회원 가입 시 데이터 유효성 검사 추가

---
+++ src/main/java/com/example/villagerservice/member/api/AuthApiController.java
@@ -0,0 +1,24 @@
+package com.example.villagerservice.member.api;
+
+import com.example.villagerservice.member.request.MemberCreate;
+import com.example.villagerservice.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthApiController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/signup")
+    public void createMember(@Valid @RequestBody MemberCreate memberCreate) {
+        memberCreate.passwordEncrypt(passwordEncoder.encode(memberCreate.getPassword()));
+        memberService.createMember(memberCreate);
+    }
+}
