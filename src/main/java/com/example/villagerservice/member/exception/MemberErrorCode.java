c18f49031b3dd688cb1771aec5076698cb2e9be0
donghun93 <alswn4516@gmail.com>
2022-12-01 00:15:07 +0900

    #23 - 회원 예외 정리
    - global 예외로 대체하고 회원 도메인에 대한 예외 추가

--- src/main/java/com/example/villagerservice/member/exception/MemberErrorCode.java
+++ src/main/java/com/example/villagerservice/member/exception/MemberErrorCode.java
@@ -1,15 +1,24 @@
 package com.example.villagerservice.member.exception;
 
+import com.example.villagerservice.common.response.ErrorResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum MemberErrorCode {
-    MEMBER_VALIDATE_ERROR("유효성 검사에 실패하였습니다."),
-    MEMBER_DUPLICATE_ERROR("회원이 중복되었습니다."),
-    MEMBER_NOT_FOUND("회원이 존재하지 않습니다."),
+    MEMBER_AUTH_NOT_VALID("INFO-400", "ID 또는 비밀번호가 일치하지 않습니다."),
+    MEMBER_DUPLICATE_ERROR("INFO-401", "회원이 중복되었습니다."),
+    MEMBER_NOT_FOUND( "INFO-402", "회원이 존재하지 않습니다."),
     ;
 
+    private final String errorCode;
     private final String errorMessage;
+
+    public static ErrorResponse createErrorResponse(MemberErrorCode errorCode) {
+        return ErrorResponse.builder()
+                .errorCode(errorCode.getErrorCode())
+                .errorMessage(errorCode.getErrorMessage())
+                .build();
+    }
 }
--- src/main/java/com/example/villagerservice/member/exception/MemberException.java
+++ src/main/java/com/example/villagerservice/member/exception/MemberException.java
@@ -1,12 +1,14 @@
 package com.example.villagerservice.member.exception;
 
+import com.example.villagerservice.common.exception.VillagerException;
 import lombok.Getter;
 
 @Getter
-public class MemberException extends RuntimeException {
-    private final MemberErrorCode errorCode;
+public class MemberException extends VillagerException {
+    private final MemberErrorCode memberErrorCode;
 
     public MemberException(MemberErrorCode errorCode) {
-        this.errorCode = errorCode;
+        super(errorCode.getErrorCode(), errorCode.getErrorMessage());
+        this.memberErrorCode = errorCode;
     }
 }
