bb989d12feec269e04119171ec7914a2f2fd8353
donghun93 <alswn4516@gmail.com>
2022-11-30 12:11:35 +0900

    #4 - 회원 도메인 개발
    - 현재 회원 가입 기능만 추가함

--- src/main/java/com/example/villagerservice/member/domain/Member.java
+++ src/main/java/com/example/villagerservice/member/domain/Member.java
@@ -1,20 +1,33 @@
 package com.example.villagerservice.member.domain;
 
+import com.example.villagerservice.common.domain.BaseTimeEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
-    private String username;
+    private String nickname;
     private String email;
     private String pass;
+    @Enumerated(EnumType.STRING)
     private RoleType roleType;
+
+    @Builder
+    private Member(String nickname, String email, String pass) {
+        this.nickname = nickname;
+        this.email = email;
+        this.pass = pass;
+        this.roleType = RoleType.USER;
+    }
 }
