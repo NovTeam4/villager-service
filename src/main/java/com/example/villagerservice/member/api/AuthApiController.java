624ee5aedd8bc8cb0b08e8b25aa734987d860095
donghun93 <alswn4516@gmail.com>
2022-12-01 15:17:32 +0900

    #23 - Auth 관련 컨트롤러 개발
    - AuthApiController는 기본적인 인증과 관련된 컨트롤러이다.
    - 현재 회원가입, 토큰 재발행 기능을 제공한다.

--- src/main/java/com/example/villagerservice/member/api/AuthApiController.java
+++ src/main/java/com/example/villagerservice/member/api/AuthApiController.java
@@ -1,12 +1,19 @@
 package com.example.villagerservice.member.api;
 
+import com.example.villagerservice.common.jwt.JwtTokenException;
+import com.example.villagerservice.common.jwt.JwtTokenInfoDto;
+import com.example.villagerservice.common.jwt.JwtTokenResponse;
 import com.example.villagerservice.member.request.MemberCreate;
+import com.example.villagerservice.member.service.AuthTokenService;
 import com.example.villagerservice.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static com.example.villagerservice.common.jwt.JwtTokenErrorCode.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,11 +21,26 @@ import javax.validation.Valid;
 public class AuthApiController {
 
     private final MemberService memberService;
+    private final AuthTokenService authTokenService;
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signup")
     public void createMember(@Valid @RequestBody MemberCreate memberCreate) {
         memberCreate.passwordEncrypt(passwordEncoder.encode(memberCreate.getPassword()));
         memberService.createMember(memberCreate);
+    }
+
+    @PostMapping("/refresh")
+    public JwtTokenResponse reissue(@RequestHeader("access-token") String accessToken,
+                                                    @RequestHeader("refresh-token") String refreshToken) {
+        if (!StringUtils.hasText(accessToken)) {
+            throw new JwtTokenException(JWT_ACCESS_TOKEN_NOT_EXIST);
+        }
+        if (!StringUtils.hasText(refreshToken)) {
+            throw new JwtTokenException(JWT_REFRESH_TOKEN_NOT_EXIST);
+        }
+
+        JwtTokenInfoDto jwtTokenInfoDto = authTokenService.getReissueTokenInfo(accessToken, refreshToken);
+        return JwtTokenResponse.createResponseBody(jwtTokenInfoDto);
     }
 }
