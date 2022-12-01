51b040b63b570471d6a2a0816be81920ddeedeb5
donghun93 <alswn4516@gmail.com>
2022-12-01 21:18:39 +0900

    #9 - 회원 정보 변경 서비스 개발
    - 회원 정보 변경 로직 개발 (현재는 닉네임만 변경 가능, 변경 데이터는 나중에 점점 추가될 예정)
    - 회원 비밀번호 변경 로직 개발

--- src/main/java/com/example/villagerservice/member/service/MemberService.java
+++ src/main/java/com/example/villagerservice/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.example.villagerservice.member.service;
 
 import com.example.villagerservice.member.request.MemberCreate;
+import com.example.villagerservice.member.request.MemberInfoUpdate;
 
 public interface MemberService {
     void createMember(MemberCreate memberCreate);
+
+    void updateMemberInfo(String email, MemberInfoUpdate memberInfoUpdate);
+    void updateMemberPassword(String email, String pass);
 }
