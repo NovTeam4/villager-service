cb79f23fe402601be638d92b25ca84c76f55c443
donghun93 <alswn4516@gmail.com>
2022-12-01 15:11:15 +0900

    #23 - redis config 추가
    - redis config 추가, yml은 우선 개인적으로 관리하고 추후에 yml 관련 암호화 처리할 예정
    - 기본적으로 redis 통신은 RedisTemplate으로 통신한다. RedisRepository는 쉽게 refreshToken을 처리하기 위해 개발했다.

---
+++ src/main/java/com/example/villagerservice/config/redis/RedisRepository.java
@@ -0,0 +1,10 @@
+package com.example.villagerservice.config.redis;
+
+import com.example.villagerservice.common.jwt.JwtTokenInfoDto;
+import org.springframework.security.core.Authentication;
+
+public interface RedisRepository {
+    void saveRefreshToken(Authentication authentication, JwtTokenInfoDto value);
+
+    String findRefreshToken(Authentication authentication);
+}
