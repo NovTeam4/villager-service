2e5dd55d2969100e5b2728d247d897a690ad7642
donghun93 <alswn4516@gmail.com>
2022-12-01 15:13:58 +0900

    #23 - 토큰 재발행 서비스 개발
    - client로부터 토큰 재발행 요청이 올 경우 처리할 서비스 오브젝트이다.
    - token에 대한 유효성 검사를 진행하나, 만료된 예외는 무시하고 넘어간다.

---
+++ src/main/java/com/example/villagerservice/member/service/impl/AuthTokenServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.villagerservice.member.service.impl;
+
+import com.example.villagerservice.common.jwt.JwtTokenException;
+import com.example.villagerservice.common.jwt.JwtTokenProvider;
+import com.example.villagerservice.common.jwt.JwtTokenInfoDto;
+import com.example.villagerservice.common.jwt.JwtTokenType;
+import com.example.villagerservice.config.redis.RedisRepository;
+import com.example.villagerservice.member.service.AuthTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import static com.example.villagerservice.common.jwt.JwtTokenErrorCode.JWT_REFRESH_TOKEN_NOT_SAME;
+import static com.example.villagerservice.common.jwt.JwtTokenType.ACCESS_TOKEN;
+import static com.example.villagerservice.common.jwt.JwtTokenType.REFRESH_TOKEN;
+
+@Service
+@RequiredArgsConstructor
+public class AuthTokenServiceImpl implements AuthTokenService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final RedisRepository redisRepository;
+
+    @Override
+    public JwtTokenInfoDto getReissueTokenInfo(String accessToken, String refreshToken) {
+        jwtTokenProvider.validateToken(accessToken, ACCESS_TOKEN);
+        jwtTokenProvider.validateToken(refreshToken, REFRESH_TOKEN);
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+
+        String findRefreshToken = redisRepository.findRefreshToken(authentication);
+        if (!refreshToken.equals(findRefreshToken)) {
+            throw new JwtTokenException(JWT_REFRESH_TOKEN_NOT_SAME);
+        }
+
+        JwtTokenInfoDto jwtTokenInfoDto = jwtTokenProvider.generateToken(authentication);
+        redisRepository.saveRefreshToken(authentication, jwtTokenInfoDto);
+        return jwtTokenInfoDto;
+    }
+}
