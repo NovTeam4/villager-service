6b195d139734fa09eede44890f439098a4ad1b50
donghun93 <alswn4516@gmail.com>
2022-12-01 21:20:35 +0900

    #9 - 회원 정보 변경 요청 오브젝트 추가
    - 회원 정보 변경 오브젝트 추가
    - 회원 비밀번호 변경 오브젝트 추가
    - 비밀번호 유효성 검사 Custom Validation 추가(@Password)

---
+++ src/main/java/com/example/villagerservice/member/valid/PasswordValidator.java
@@ -0,0 +1,14 @@
+package com.example.villagerservice.member.valid;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<Password, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return false;
+        }
+        return value.matches("(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}");
+    }
+}
