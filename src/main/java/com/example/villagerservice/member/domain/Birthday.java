674f816b8ffbf78c99570e94518baa3b8860e299
donghun93 <alswn4516@gmail.com>
2022-12-03 16:10:47 +0900

    #11 - 회원 도메인 필드 추가
    - 회원 도메인에 생년월일, 성별, 매너점수 필드 추가

---
+++ src/main/java/com/example/villagerservice/member/domain/Birthday.java
@@ -0,0 +1,25 @@
+package com.example.villagerservice.member.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Birthday {
+    @Column(name = "birth_year", length = 4)
+    private int year;
+    @Column(name = "birth_month", length = 2)
+    private int month;
+    @Column(name = "birth_day", length = 2)
+    private int day;
+    public Birthday(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
