b63f84f98236d38f08252a47d1ca23423ba4cb01
donghun93 <alswn4516@gmail.com>
2022-12-07 12:53:11 +0900

    회원가입 api 컨트롤러 변경
    - 통합 테스트 진행함
    - @Import({TestConfig.class}) 테스트 환경에서 진행
    - BaseDocumentation 문서 작업하는 부모 클래스 상속
    - 문서 작성을 위해 기존 테스트 코드 수정

--- src/test/java/com/example/villagerservice/member/api/AuthApiControllerTest.java
+++ src/test/java/com/example/villagerservice/member/api/AuthApiControllerTest.java
@@ -1,66 +1,32 @@
 package com.example.villagerservice.member.api;
 
-import com.example.villagerservice.BaseControllerTest;
-import com.example.villagerservice.common.jwt.JwtTokenProvider;
+import com.example.document.member.CreateMemberDocumentationTemplate;
+import com.example.document.BaseDocumentation;
+import com.example.villagerservice.TestConfig;
 import com.example.villagerservice.member.domain.Gender;
-import com.example.villagerservice.member.request.MemberCreate;
+import com.example.villagerservice.member.dto.CreateMember;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.restdocs.snippet.Snippet;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static com.example.villagerservice.common.exception.CommonErrorCode.DATA_INVALID_ERROR;
-import static io.restassured.RestAssured.given;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@ActiveProfiles({"test"})
-class AuthApiControllerTest extends BaseControllerTest {
-
-    @TestConfiguration
-    static class AuthApiControllerTestConfig {
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .csrf()
-                    .disable();
-
-            return http.build();
-        }
-        @MockBean
-        private JwtTokenProvider jwtTokenProvider;
-
-        @MockBean
-        private PasswordEncoder passwordEncoder;
-    }
-
-    @Autowired
-    private MockMvc mockMvc;
+@Import({TestConfig.class})
+class AuthApiControllerTest extends BaseDocumentation {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    private CreateMemberDocumentationTemplate template = new CreateMemberDocumentationTemplate();
 
     @Test
     @DisplayName("닉네임을 전달하지 않을 경우 테스트")
@@ -69,31 +35,24 @@ class AuthApiControllerTest extends BaseControllerTest {
         String memberJson = createMember("    ", "test@gamil.com", "Shwi18a78!!",
                 Gender.MAN, 2022, 12, 5);
 
+        String errorKey = "validation.nickname";
+        String errorDescription = "닉네임은 필수입력 값입니다.";
         // when & then
-//        mockMvc.perform(post("/api/v1/auth/signup")
-//                        .contentType(APPLICATION_JSON)
-//                        .content(memberJson)
-//                )
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.errorCode").value(DATA_INVALID_ERROR.getErrorCode()))
-//                .andExpect(jsonPath("$.errorMessage").value(DATA_INVALID_ERROR.getErrorMessage()))
-//                .andExpect(jsonPath("$.validation.nickname").value("닉네임은 필수입력 값입니다."))
-//                .andDo(print());
-
-
-        given(this.spec)
-                .filter(document(DEFAULT_RESTDOC_PATH)) // API 문서 관련 필터 추가
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .header("Content-type", "application/json")
-                .body(memberJson)
-                .log().all()
+        baseGiven(memberJson,
+                template.createRestDocumentation(
+                        "회원가입",
+                        "닉네임 유효성 검사 실패",
+                        errorKey, errorDescription)
+        )
                 .when()
                 .post("/api/v1/auth/signup")
                 .then()
                 .statusCode(HttpStatus.OK.value())
                 .body("errorCode", Matchers.equalTo(DATA_INVALID_ERROR.getErrorCode()))
-                .body("errorMessage", Matchers.equalTo(DATA_INVALID_ERROR.getErrorMessage()));
+                .body("errorMessage", Matchers.equalTo(DATA_INVALID_ERROR.getErrorMessage()))
+                .body("validation.nickname", Matchers.equalTo("닉네임은 필수입력 값입니다."));
     }
+
 
     @ParameterizedTest
     @ValueSource(strings = {"", "     ", "notformat@@"})
@@ -102,17 +61,23 @@ class AuthApiControllerTest extends BaseControllerTest {
         // given
         String memberJson = createMember("nickname1", email, "Shwi18a78!!",
                 Gender.MAN, 2022, 12, 5);
+        String errorKey = "validation.email";
+        String errorDescription = "유효하지 않은 이메일 형식입니다.";
 
         // when & then
-        mockMvc.perform(post("/api/v1/auth/signup")
-                        .contentType(APPLICATION_JSON)
-                        .content(memberJson)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.errorCode").value(DATA_INVALID_ERROR.getErrorCode()))
-                .andExpect(jsonPath("$.errorMessage").value(DATA_INVALID_ERROR.getErrorMessage()))
-                .andExpect(jsonPath("$.validation.email").value("유효하지 않은 이메일 형식입니다."))
-                .andDo(print());
+        baseGiven(memberJson,
+                template.createRestDocumentation(
+                        "회원가입",
+                        "이메일 유효성 검사 실패",
+                        errorKey, errorDescription)
+        )
+                .when()
+                .post("/api/v1/auth/signup")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("errorCode", Matchers.equalTo(DATA_INVALID_ERROR.getErrorCode()))
+                .body("errorMessage", Matchers.equalTo(DATA_INVALID_ERROR.getErrorMessage()))
+                .body(errorKey, Matchers.equalTo(errorDescription));
     }
 
     @ParameterizedTest
@@ -123,28 +88,25 @@ class AuthApiControllerTest extends BaseControllerTest {
         String memberJson = createMember("nickname1", "test@gmail.com", password,
                 Gender.MAN, 2022, 12, 5);
 
+        String errorKey = "validation.password";
+        String errorDescription = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+
         // when & then
-        mockMvc.perform(post("/api/v1/auth/signup")
-                        .contentType(APPLICATION_JSON)
-                        .content(memberJson)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.errorCode").value(DATA_INVALID_ERROR.getErrorCode()))
-                .andExpect(jsonPath("$.errorMessage").value(DATA_INVALID_ERROR.getErrorMessage()))
-                .andExpect(jsonPath("$.validation.password").value("비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요."))
-                .andDo(print());
+
+        baseGiven(memberJson,
+                template.createRestDocumentation(
+                        "회원가입",
+                        "비밀번호 유효성 검사 실패",
+                        errorKey, errorDescription)
+        )
+                .when()
+                .post("/api/v1/auth/signup")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("errorCode", Matchers.equalTo(DATA_INVALID_ERROR.getErrorCode()))
+                .body("errorMessage", Matchers.equalTo(DATA_INVALID_ERROR.getErrorMessage()))
+                .body(errorKey, Matchers.equalTo(errorDescription));
     }
-
-    private static final Snippet REQUEST_FIELDS = requestFields(
-            fieldWithPath("nickname").type(JsonFieldType.STRING).description("별칭"),
-            fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
-            fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
-            fieldWithPath("gender").type(JsonFieldType.STRING).description("성별"),
-            fieldWithPath("year").type(JsonFieldType.NUMBER).description("년도"),
-            fieldWithPath("month").type(JsonFieldType.NUMBER).description("달"),
-            fieldWithPath("day").type(JsonFieldType.NUMBER).description("일")
-    );
-
 
     @Test
     @DisplayName("회원가입 테스트")
@@ -154,19 +116,10 @@ class AuthApiControllerTest extends BaseControllerTest {
                 Gender.MAN, 2022, 12, 5);
 
         // when & then
-//        mockMvc.perform(post("/api/v1/auth/signup")
-//                        .contentType(APPLICATION_JSON)
-//                        .content(memberJson)
-//                )
-//                .andExpect(status().isOk())
-//                .andDo(print());
-
-        given(this.spec)
-                .filter(document(DEFAULT_RESTDOC_PATH, REQUEST_FIELDS)) // API 문서 관련 필터 추가
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .header("Content-type", "application/json")
-                .body(memberJson)
-                .log().all()
+        baseGiven(memberJson,
+                template.createRestDocumentation(
+                        "회원가입",
+                        "회원가입 성공"))
                 .when()
                 .post("/api/v1/auth/signup")
                 .then()
@@ -174,9 +127,10 @@ class AuthApiControllerTest extends BaseControllerTest {
     }
 
 
+    @NotNull
     private String createMember(String nickname, String email, String pass, Gender gender,
                                 int year, int month, int day) throws JsonProcessingException {
-        MemberCreate memberCreate = MemberCreate.builder()
+        CreateMember.Request memberCreate = CreateMember.Request.builder()
                 .nickname(nickname)
                 .email(email)
                 .password(pass)
