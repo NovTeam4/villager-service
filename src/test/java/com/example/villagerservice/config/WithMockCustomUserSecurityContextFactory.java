1eff7a3172c0c1602e7c3d926dcf9be4bce68930
donghun93 <alswn4516@gmail.com>
2022-12-08 12:42:49 +0900

    테스트 코드 리팩토링
    - api 문서 만드는 컨트롤러 테스트는 통합 테스트로 진행함
    - 필요에 따라 RestDocumentationTemplate에 문서 양식 추가 해야함
    - AuthConfig는 인증 관련 필터를 사용하기 위한 환경
    - TestConfig는 인증 관련 필터를 사용하지 않는 환경
    - @WithMockCustomMember를 사용하면 기본적인 회원이 Authentication에 들어감

---
+++ src/test/java/com/example/villagerservice/config/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,23 @@
+package com.example.villagerservice.config;
+
+import com.example.villagerservice.member.domain.Member;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomMember> {
+    @Override
+    public SecurityContext createSecurityContext(WithMockCustomMember annotation) {
+        final SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+        final Member member = Member.builder().email("test").build();
+        member.setJwtMemberId(1L);
+
+        final UsernamePasswordAuthenticationToken authenticationToken
+                = new UsernamePasswordAuthenticationToken(member, "password", null);
+
+        securityContext.setAuthentication(authenticationToken);
+        return securityContext;
+    }
+}
--- src/test/java/com/example/villagerservice/member/api/MemberApiControllerTest.java
+++ src/test/java/com/example/villagerservice/member/api/MemberApiControllerTest.java
@@ -1,96 +1,54 @@
 package com.example.villagerservice.member.api;
 
-import com.example.villagerservice.common.jwt.JwtTokenProvider;
-import com.example.villagerservice.member.domain.Member;
-import com.example.villagerservice.member.domain.MemberRepository;
-import com.example.villagerservice.member.exception.MemberException;
+import com.example.villagerservice.config.WithMockCustomMember;
 import com.example.villagerservice.member.dto.CreateMemberAttentionTag;
 import com.example.villagerservice.member.dto.UpdateMemberInfo;
 import com.example.villagerservice.member.dto.UpdateMemberPassword;
+import com.example.villagerservice.member.service.MemberService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static com.example.villagerservice.common.exception.CommonErrorCode.DATA_INVALID_ERROR;
-import static com.example.villagerservice.member.exception.MemberErrorCode.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
-@SpringBootTest
-@ActiveProfiles({"test"})
-@Transactional
+
+@WebMvcTest(MemberApiController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class MemberApiControllerTest {
-
-    @TestConfiguration
-    static class AuthApiControllerTestConfig {
-
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .csrf()
-                    .disable();
-
-            return http.build();
-        }
-
-        @MockBean
-        private JwtTokenProvider jwtTokenProvider;
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-
-    }
-
+    @MockBean
+    private MemberService memberService;
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @Test
-    @DisplayName("회원 정보 변경 시 회원이 없을 경우 테스트")
+    @WithMockCustomMember
+    @DisplayName("회원 정보 잘못 넘겼을 경우 테스트")
     void updateMemberInfoNotFoundMemberTest() throws Exception {
         // given
         UpdateMemberInfo.Request updateMemberInfo = UpdateMemberInfo.Request.builder()
-                .nickname("닉네임 변경!")
+                .nickname("")
                 .build();
         String jsonMemberInfoUpdate = objectMapper.writeValueAsString(updateMemberInfo);
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(Member.builder()
-                .email("hello@naver.com")
-                .nickname("변경전 닉네임!")
-                .build(), null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
         // when & then
         mockMvc.perform(patch("/api/v1/members/info")
@@ -98,28 +56,25 @@ class MemberApiControllerTest {
                         .content(jsonMemberInfoUpdate)
                 )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.errorCode").value(MEMBER_NOT_FOUND.getErrorCode()))
-                .andExpect(jsonPath("$.errorMessage").value(MEMBER_NOT_FOUND.getErrorMessage()))
+                .andExpect(jsonPath("$.errorCode").value(DATA_INVALID_ERROR.getErrorCode()))
+                .andExpect(jsonPath("$.errorMessage").value(DATA_INVALID_ERROR.getErrorMessage()))
+                .andExpect(jsonPath("$.validation.nickname").value("닉네임은 필수입력 값입니다."))
                 .andDo(print());
     }
 
     @Test
+    @WithMockCustomMember
     @DisplayName("회원 정보 변경 테스트")
     void updateMemberInfoTest() throws Exception {
         // given
-        Member member = Member.builder()
-                .email("hello@naver.com")
-                .nickname("변경전 닉네임!")
-                .build();
-        memberRepository.save(member);
-
         UpdateMemberInfo.Request updateMemberInfo = UpdateMemberInfo.Request.builder()
                 .nickname("닉네임 변경!")
                 .build();
         String jsonMemberInfoUpdate = objectMapper.writeValueAsString(updateMemberInfo);
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(member, null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        doNothing()
+                .when(memberService)
+                .updateMemberInfo(anyString(), any());
 
         // when & then
         mockMvc.perform(patch("/api/v1/members/info")
@@ -129,90 +84,18 @@ class MemberApiControllerTest {
                 .andExpect(status().isOk())
                 .andDo(print());
 
-        Member findMember = memberRepository.findByEmail("hello@naver.com")
-                .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
-
-        assertThat(findMember.getMemberDetail().getNickname()).isEqualTo("닉네임 변경!");
+        verify(memberService, times(1)).updateMemberInfo(anyString(), any());
     }
 
-    @Test
-    @DisplayName("회원 비밀번호 변경 시 회원이 없을 경우 테스트")
-    void updateMemberPasswordNotFoundMemberTest() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "sid71@N", "sid71@Nsid71@Nsid71@N"})
+    @DisplayName("비밀번호 변경시 인자 잘못 넘겼을 경우 테스트")
+    void updateMemberPasswordNotFoundMemberTest(String password) throws Exception {
         // given
-        UpdateMemberPassword.Request updateMemberPassword = UpdateMemberPassword.Request.builder()
-                .password("new!!Password@1")
-                .build();
-        String jsonMemberInfoUpdate = objectMapper.writeValueAsString(updateMemberPassword);
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(Member.builder()
-                .email("hello@naver.com")
-                .nickname("변경전 닉네임!")
-                .build(), null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-        // when & then
-        mockMvc.perform(patch("/api/v1/members/password")
-                        .contentType(APPLICATION_JSON)
-                        .content(jsonMemberInfoUpdate)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.errorCode").value(MEMBER_NOT_FOUND.getErrorCode()))
-                .andExpect(jsonPath("$.errorMessage").value(MEMBER_NOT_FOUND.getErrorMessage()))
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("회원 비밀번호 변경 시 비밀번호가 동일한 경우 테스트")
-    void updateMemberPasswordSamePasswordTest() throws Exception {
-        // given
-        String password = "new!!Password@1";
-        Member member = Member.builder()
-                .email("hello@naver.com")
-                .nickname("변경전 닉네임!")
-                .encodedPassword(passwordEncoder.encode(password))
-                .build();
-        memberRepository.save(member);
-
         UpdateMemberPassword.Request updateMemberPassword = UpdateMemberPassword.Request.builder()
                 .password(password)
                 .build();
         String jsonMemberInfoUpdate = objectMapper.writeValueAsString(updateMemberPassword);
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(member, null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-        // when & then
-        mockMvc.perform(patch("/api/v1/members/password")
-                        .contentType(APPLICATION_JSON)
-                        .content(jsonMemberInfoUpdate)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.errorCode").value(MEMBER_UPDATE_SAME_PASS.getErrorCode()))
-                .andExpect(jsonPath("$.errorMessage").value(MEMBER_UPDATE_SAME_PASS.getErrorMessage()))
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("회원 비밀번호 변경 시 숫자 없을 경우 테스트")
-    void updateMemberPasswordValidTest() throws Exception {
-        // given
-        String password = "default!!Password@1";
-        String newPassword = "new!!Password@";
-        Member member = Member.builder()
-                .email("hello@naver.com")
-                .nickname("변경전 닉네임!")
-                .encodedPassword(passwordEncoder.encode(password))
-                .build();
-        memberRepository.save(member);
-
-        UpdateMemberPassword.Request updateMemberPassword = UpdateMemberPassword.Request.builder()
-                .password(newPassword)
-                .build();
-
-        String jsonMemberInfoUpdate = objectMapper.writeValueAsString(updateMemberPassword);
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(member, null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
         // when & then
         mockMvc.perform(patch("/api/v1/members/password")
@@ -222,30 +105,23 @@ class MemberApiControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.errorCode").value(DATA_INVALID_ERROR.getErrorCode()))
                 .andExpect(jsonPath("$.errorMessage").value(DATA_INVALID_ERROR.getErrorMessage()))
+                .andExpect(jsonPath("$.validation.password").value("비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요."))
                 .andDo(print());
     }
 
     @Test
-    @DisplayName("회원 비밀번호 테스트")
+    @WithMockCustomMember
+    @DisplayName("비밀번호 변경 테스트")
     void updateMemberPasswordTest() throws Exception {
         // given
-        String password = "default!!Password@1";
-        String newPassword = "new!!Password@12";
-        Member member = Member.builder()
-                .email("hello@naver.com")
-                .nickname("변경전 닉네임!")
-                .encodedPassword(passwordEncoder.encode(password))
-                .build();
-        memberRepository.save(member);
-
         UpdateMemberPassword.Request updateMemberPassword = UpdateMemberPassword.Request.builder()
-                .password(newPassword)
+                .password("ajkls12@aks!")
                 .build();
-
         String jsonMemberInfoUpdate = objectMapper.writeValueAsString(updateMemberPassword);
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(member, null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        doNothing()
+                .when(memberService)
+                .updateMemberPassword(anyString(), any());
 
         // when & then
         mockMvc.perform(patch("/api/v1/members/password")
@@ -255,108 +131,69 @@ class MemberApiControllerTest {
                 .andExpect(status().isOk())
                 .andDo(print());
 
-        Member findMember = memberRepository.findByEmail("hello@naver.com")
-                .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
-
-        // then
-        assertThat(passwordEncoder.matches(newPassword, findMember.getEncodedPassword())).isTrue();
+        verify(memberService, times(1)).updateMemberPassword(anyString(), any());
     }
 
     @Test
+    @WithMockCustomMember
     @DisplayName("회원 탈퇴 테스트")
     void deleteMemberTest() throws Exception {
-        Member member = Member.builder()
-                .email("hello@naver.com")
-                .nickname("hello")
-                .encodedPassword(passwordEncoder.encode("default!!Password@1"))
-                .build();
-        memberRepository.save(member);
+        // given
+        doNothing()
+                .when(memberService)
+                .deleteMember(anyString());
 
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(member, null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-        // when
+        // when & then
         mockMvc.perform(delete("/api/v1/members"))
                 .andExpect(status().isOk())
                 .andDo(print());
 
-        Member findMember = memberRepository.findByEmail("hello@naver.com")
-                .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
-
-        // then
-        assertThat(findMember.isDeleted()).isTrue();
+        verify(memberService, times(1)).deleteMember(anyString());
     }
 
     @Test
-    @DisplayName("관심태그 추가 시 5개 넘을경우 테스트")
-    void addMemberAttentionTagMaxOverTest() throws Exception {
+    @DisplayName("관심태그 추가 시 하나도 입력 안한 경우 테스트")
+    void addMemberAttentionTagEmptyTest() throws Exception {
         // given
-        Member member = Member.builder()
-                .email("hello@naver.com")
-                .nickname("hello")
-                .encodedPassword(passwordEncoder.encode("default!!Password@1"))
+        CreateMemberAttentionTag.Request tags = CreateMemberAttentionTag.Request.builder()
+                .tags(new ArrayList<>())
                 .build();
-
-        memberRepository.save(member);
-
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(member, null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-        CreateMemberAttentionTag.Request createMemberAttentionTag = CreateMemberAttentionTag.Request.builder()
-                .tags(Arrays.asList("#봄", "#여름", "#가을", "#겨울", "#테스트", "태그"))
-                .build();
-
-        String jsonMemberAddAttentionTag =
-                objectMapper.writeValueAsString(createMemberAttentionTag);
+        String jsonMemberInfoUpdate = objectMapper.writeValueAsString(tags);
 
         // when & then
         mockMvc.perform(post("/api/v1/members/tags")
                         .contentType(APPLICATION_JSON)
-                        .content(jsonMemberAddAttentionTag))
+                        .content(jsonMemberInfoUpdate)
+                )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.errorCode").value(MEMBER_TAG_MAX_COUNT.getErrorCode()))
-                .andExpect(jsonPath("$.errorMessage").value(MEMBER_TAG_MAX_COUNT.getErrorMessage()))
+                .andExpect(jsonPath("$.errorCode").value(DATA_INVALID_ERROR.getErrorCode()))
+                .andExpect(jsonPath("$.errorMessage").value(DATA_INVALID_ERROR.getErrorMessage()))
+                .andExpect(jsonPath("$.validation.tags").value("태그를 최소 하나 이상 입력해주세요."))
                 .andDo(print());
     }
 
     @Test
-    @DisplayName("관심태그 테스트")
+    @WithMockCustomMember
+    @DisplayName("관심태그 추가 테스트")
     void addMemberAttentionTagTest() throws Exception {
         // given
-        Member member = Member.builder()
-                .email("hello@naver.com")
-                .nickname("hello")
-                .encodedPassword(passwordEncoder.encode("default!!Password@1"))
+        CreateMemberAttentionTag.Request tags = CreateMemberAttentionTag.Request.builder()
+                .tags(Arrays.asList("#바다", "#겨울"))
                 .build();
+        String jsonMemberInfoUpdate = objectMapper.writeValueAsString(tags);
 
-        memberRepository.save(member);
+        doNothing()
+                .when(memberService)
+                .addAttentionTag(anyString(), any());
 
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(member, null, null);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-        CreateMemberAttentionTag.Request createMemberAttentionTag = CreateMemberAttentionTag.Request.builder()
-                .tags(Arrays.asList("#봄", "#여름", "#가을", "#겨울", "#계절"))
-                .build();
-
-        String jsonMemberAddAttentionTag =
-                objectMapper.writeValueAsString(createMemberAttentionTag);
-
-        // when
+        // when & then
         mockMvc.perform(post("/api/v1/members/tags")
                         .contentType(APPLICATION_JSON)
-                        .content(jsonMemberAddAttentionTag))
+                        .content(jsonMemberInfoUpdate)
+                )
                 .andExpect(status().isOk())
                 .andDo(print());
 
-        // then
-        assertThat(member.getTagCollection().getTagCount()).isEqualTo(5);
-        assertThat(member.getTagCollection().getTags().get(0).getName()).isEqualTo("#봄");
-        assertThat(member.getTagCollection().getTags().get(1).getName()).isEqualTo("#여름");
-        assertThat(member.getTagCollection().getTags().get(2).getName()).isEqualTo("#가을");
-        assertThat(member.getTagCollection().getTags().get(3).getName()).isEqualTo("#겨울");
-        assertThat(member.getTagCollection().getTags().get(4).getName()).isEqualTo("#계절");
+        verify(memberService, times(1)).addAttentionTag(anyString(), any());
     }
 }
