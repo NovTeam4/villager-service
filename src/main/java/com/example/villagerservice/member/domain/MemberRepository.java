be2c3cf8e7e955a6c52258111d6ab1d63fb09bf3
donghun93 <alswn4516@gmail.com>
2022-12-08 15:17:19 +0900

    회원 리포지토리 태그 조회 기능 추가
    - 회원, 관심태그, 상세정보 3개 테이블 fetch join으로 전부 조회 기능 추가

--- src/main/java/com/example/villagerservice/member/domain/MemberRepository.java
+++ src/main/java/com/example/villagerservice/member/domain/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -12,4 +13,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m join fetch m.memberDetail where m.email = :email")
     Optional<Member> findByMemberDetail(@Param("email") String email);
+
+    @Query("select m from Member m " +
+            " join fetch m.tagCollection.tags " +
+            " join fetch m.memberDetail " +
+            " where m.id = :memberId")
+    Optional<Member> findByMemberWithTag(@Param("memberId") Long memberId);
 }
