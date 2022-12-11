5d08940ee18a80975cbc502c257942f01e94683f
KyeongSeopHam <rudtjq5724@gmail.com>
2022-12-12 03:20:29 +0900

    동네 게시판  변경 서비스 etc

--- src/main/java/com/example/villagerservice/post/domain/PostRepository.java
+++ src/main/java/com/example/villagerservice/post/domain/PostRepository.java
@@ -1,6 +1,11 @@
 package com.example.villagerservice.post.domain;
+import com.example.villagerservice.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
 
+ // select * post where id and member
+    Optional<Post> findByIdAndMember(Long postId,Member member);
 }
