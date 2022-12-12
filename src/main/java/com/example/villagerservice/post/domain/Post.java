30a15d28a83933a07da15e817402d594214171b0
donghun93 <alswn4516@gmail.com>
2022-12-12 22:13:51 +0900

    게시글 이미지 도메인 추가, 게시글 도메인 변경
    - 게시글 도메인 연관관계 편의 메서드 추가

--- src/main/java/com/example/villagerservice/post/domain/Post.java
+++ src/main/java/com/example/villagerservice/post/domain/Post.java
@@ -2,12 +2,17 @@ package com.example.villagerservice.post.domain;
 
 import com.example.villagerservice.common.domain.BaseTimeEntity;
 import com.example.villagerservice.member.domain.Member;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,31 +25,31 @@ public class Post extends BaseTimeEntity {
     private String contents; // 내용
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name ="member_id")
+    @JoinColumn(name = "member_id")
     private Member member;   // 회원
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name ="category_id")
+    @JoinColumn(name = "category_id")
     private Category category; // 카테고리
 
-    protected Post() {
-    }
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<PostImage> images = new ArrayList<>();
 
-
-    public Post(Member member, Category category,  String title, String contents) {
+    public Post(Member member, Category category, String title, String contents) {
         this.member = member;
-        this.category =category;
+        this.category = category;
         this.title = title;
         this.contents = contents;
     }
 
-    public void updatePost(Category category,  String title, String contents) {
-        this.category =category;
+    public void updatePost(Category category, String title, String contents) {
+        this.category = category;
         this.title = title;
         this.contents = contents;
     }
 
-
-
-
+    public void addImages(PostImage postImage) {
+        this.images.add(postImage);
+        postImage.postReference(this);
+    }
 }
