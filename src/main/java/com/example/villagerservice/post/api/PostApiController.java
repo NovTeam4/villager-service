f890107ad512d53d7eff09c958da3ea02f175606
donghun93 <alswn4516@gmail.com>
2022-12-12 22:17:18 +0900

    게시글 등록 api 수정
    - 게시글 등록 시 이미지 관련 (List<MultipartFile> 파라미터 추가

--- src/main/java/com/example/villagerservice/post/api/PostApiController.java
+++ src/main/java/com/example/villagerservice/post/api/PostApiController.java
@@ -3,23 +3,28 @@ package com.example.villagerservice.post.api;
 import com.example.villagerservice.member.domain.Member;
 import com.example.villagerservice.post.dto.CreatePost;
 import com.example.villagerservice.post.dto.UpdatePost;
+import com.example.villagerservice.post.service.FileUploadService;
 import com.example.villagerservice.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
 public class PostApiController {
     private final PostService postService;
+    private final FileUploadService fileUploadService;
 
     @PostMapping
     public void createPost(@AuthenticationPrincipal Member member,
-                           @Valid @RequestBody CreatePost.Request request) {
-        postService.createPost(member.getId(), request);
+                           @RequestPart(value = "request") CreatePost.Request request,
+                           @RequestPart(value = "files") List<MultipartFile> files) {
+        postService.createPost(member.getId(), request, files);
     }
 
     @PutMapping("/{id}")
