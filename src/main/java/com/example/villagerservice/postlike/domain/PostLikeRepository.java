a8d693b4ef8e12b48fb0715a4df5e158ebb66fb3
donghun93 <alswn4516@gmail.com>
2022-12-12 16:12:17 +0900

    게시글 좋아요 도메인 개발
    - 도메인 추가
    - 리포지토리 추가

---
+++ src/main/java/com/example/villagerservice/postlike/domain/PostLikeRepository.java
@@ -0,0 +1,6 @@
+package com.example.villagerservice.postlike.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+}
