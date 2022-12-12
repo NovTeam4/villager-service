44cc8c2a15e3fc1ac405fca64606ba3ace9daa1b
donghun93 <alswn4516@gmail.com>
2022-12-12 16:32:50 +0900

    게시글 좋아요 해제 서비스 개발

--- src/main/java/com/example/villagerservice/postlike/service/PostLikeService.java
+++ src/main/java/com/example/villagerservice/postlike/service/PostLikeService.java
@@ -2,4 +2,5 @@ package com.example.villagerservice.postlike.service;
 
 public interface PostLikeService {
     void postLikeCheck(Long memberId, Long postId);
+    void postLikeUnCheck(Long memberId, Long postId);
 }
