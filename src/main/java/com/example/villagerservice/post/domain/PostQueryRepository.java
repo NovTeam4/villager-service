aecd2fc1d12b47a67aaa77c45b0533711eecb4f3
donghun93 <alswn4516@gmail.com>
2022-12-13 19:29:53 +0900

    게시글 상세 조회 리포지터리 생성

--- src/main/java/com/example/villagerservice/post/domain/PostQueryRepository.java
+++ src/main/java/com/example/villagerservice/post/domain/PostQueryRepository.java
@@ -1,11 +1,12 @@
 package com.example.villagerservice.post.domain;
 
-import com.example.villagerservice.post.dto.ListPost;
 import com.example.villagerservice.post.dto.ListPostItem;
 import com.example.villagerservice.post.dto.ListPostSearchCond;
+import com.example.villagerservice.post.dto.PostItemDetail;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PostQueryRepository {
     Page<ListPostItem> getPostList(ListPostSearchCond cond, Pageable pageable);
+    PostItemDetail getPostItemDetail(Long postId);
 }
