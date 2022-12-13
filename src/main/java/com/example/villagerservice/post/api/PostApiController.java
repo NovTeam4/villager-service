a4e9a4db01aaa14e4e881ded8012103f77f5f528
donghun93 <alswn4516@gmail.com>
2022-12-13 15:29:57 +0900

    게시글 목록 조회 api 개발
    - /api/v1/posts GET Mapping , Pageable 사용

--- src/main/java/com/example/villagerservice/post/api/PostApiController.java
+++ src/main/java/com/example/villagerservice/post/api/PostApiController.java
@@ -1,11 +1,13 @@
 package com.example.villagerservice.post.api;
 
 import com.example.villagerservice.member.domain.Member;
-import com.example.villagerservice.post.dto.CreatePost;
-import com.example.villagerservice.post.dto.UpdatePost;
-import com.example.villagerservice.post.service.FileUploadService;
+import com.example.villagerservice.post.dto.*;
+import com.example.villagerservice.post.service.PostQueryService;
 import com.example.villagerservice.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +20,13 @@ import java.util.List;
 @RequestMapping("/api/v1/posts")
 public class PostApiController {
     private final PostService postService;
+    private final PostQueryService postQueryService;
+
+    @GetMapping
+    public Page<ListPostItem> getPostList(@ModelAttribute ListPostSearchCond searchCond,
+                                          @PageableDefault Pageable pageable) {
+        return postQueryService.getPostList(searchCond, pageable);
+    }
 
     @PostMapping
     public void createPost(@AuthenticationPrincipal Member member,
@@ -36,7 +45,7 @@ public class PostApiController {
     @DeleteMapping("/{id}")
     public void deletePost(@AuthenticationPrincipal Member member,
                            @PathVariable("id") Long postId
-                           ) {
+    ) {
         postService.deletePost(member.getId(), postId);
     }
 }
