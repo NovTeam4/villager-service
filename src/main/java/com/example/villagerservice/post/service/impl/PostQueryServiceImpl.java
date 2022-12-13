144cbfbc7583d1d74691dfe3d30bc8f4fc980183
donghun93 <alswn4516@gmail.com>
2022-12-13 19:24:54 +0900

    게시글 상세 조회 서비스 개발

--- src/main/java/com/example/villagerservice/post/service/impl/PostQueryServiceImpl.java
+++ src/main/java/com/example/villagerservice/post/service/impl/PostQueryServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.villagerservice.post.service.impl;
 import com.example.villagerservice.post.domain.PostQueryRepository;
 import com.example.villagerservice.post.dto.ListPostItem;
 import com.example.villagerservice.post.dto.ListPostSearchCond;
+import com.example.villagerservice.post.dto.PostItemDetail;
 import com.example.villagerservice.post.service.PostQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostQueryServiceImpl implements PostQueryService {
-
     private final PostQueryRepository postQueryRepository;
 
     @Override
     public Page<ListPostItem> getPostList(ListPostSearchCond cond, Pageable pageable) {
         return postQueryRepository.getPostList(cond, pageable);
+    }
+
+    @Override
+    public PostItemDetail getPostItemDetail(Long postId) {
+        return postQueryRepository.getPostItemDetail(postId);
     }
 }
