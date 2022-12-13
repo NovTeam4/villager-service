2c8b0a126210ccb7fd25479a9be80bbc9afd4409
donghun93 <alswn4516@gmail.com>
2022-12-14 01:32:26 +0900

    글로벌 예외 추가
    - 공용 에러 코드 추가

--- src/main/java/com/example/villagerservice/common/exception/CommonErrorCode.java
+++ src/main/java/com/example/villagerservice/common/exception/CommonErrorCode.java
@@ -9,7 +9,9 @@ public enum CommonErrorCode {
     SERVER_INVALID_ERROR("INFO-000", "INTERNAL SERVER ERROR"),
     DATA_INVALID_ERROR("INFO-100", "유효성 검사에 실패하였습니다."),
     HTTP_REQUEST_METHOD_NOT_SUPPORTED_ERROR("INFO-101", "요청 URL을 확인해주세요."),
-    HTTP_MESSAGE_NOT_READABLE("INFO-102" , "요청 인자를 확인해주세요.");
+    HTTP_MESSAGE_NOT_READABLE("INFO-102" , "요청 인자를 확인해주세요."),
+    MAX_UPLOAD_SIZE_EXCEEDED("INFO-103" , "파일 최대 사이즈(10MB)를 초과하였습니다.")
+
     ;
 
     private final String errorCode;
