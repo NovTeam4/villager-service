d1287ac3eb64bb907a52d96c1bccd8cb909b681a
KyeongSeopHam <rudtjq5724@gmail.com>
2022-12-13 23:55:14 +0900

    동네 게시판 댓글수정 인터페이스

--- src/main/java/com/example/villagerservice/post/service/PostCommentService.java
+++ src/main/java/com/example/villagerservice/post/service/PostCommentService.java
@@ -1,9 +1,11 @@
 package com.example.villagerservice.post.service;
 
 import com.example.villagerservice.post.dto.CreatePostComment;
+import com.example.villagerservice.post.dto.UpdatePostComment;
 
 public interface PostCommentService {
     void createPostComment(Long memberId, Long postId, CreatePostComment.Request request);
-    void deleteComment(Long memberId, Long postId, Long commentId);
+    void deletePostComment(Long memberId, Long postId, Long commentId);
+    void updatePostComment(Long memberId, Long postId, Long commentId, UpdatePostComment.Request request);
 
 }
