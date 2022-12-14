e1e45ae392e96e6d25f4f2373cef5d05fd597957
KyeongSeopHam <rudtjq5724@gmail.com>
2022-12-15 00:43:55 +0900

    동네 게시글 카테고리 전체조회 레파지토리

--- src/main/java/com/example/villagerservice/post/domain/CategoryRepository.java
+++ src/main/java/com/example/villagerservice/post/domain/CategoryRepository.java
@@ -2,8 +2,11 @@ package com.example.villagerservice.post.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
-
+    @Query("select c from Category c where c.isVisible=true")
+    List<Category> getCategoryList();
 }
+
