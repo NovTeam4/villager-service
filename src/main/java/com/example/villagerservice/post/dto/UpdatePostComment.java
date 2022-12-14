5c7e8367c415e11abed65a8b411080c9cc12a1d1
donghun93 <alswn4516@gmail.com>
2022-12-14 15:57:22 +0900

    게시글 댓글 변경 통합 테스트 작성

--- src/main/java/com/example/villagerservice/post/dto/UpdatePostComment.java
+++ src/main/java/com/example/villagerservice/post/dto/UpdatePostComment.java
@@ -1,6 +1,7 @@
 package com.example.villagerservice.post.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ public class UpdatePostComment {
     @NoArgsConstructor
     @AllArgsConstructor
     @Getter
+    @Builder
     public static class Request{
         @Size(min = 1, max =100, message = "댓글은 1~100글자 이내에 입력해주세요.")
         private String comment;
