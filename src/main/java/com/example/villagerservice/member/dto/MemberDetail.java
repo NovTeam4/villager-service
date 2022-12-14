ea8d05ef24752b192b10bc0d257d77d154cf2940
donghun93 <alswn4516@gmail.com>
2022-12-14 20:49:37 +0900

    회원 마이페이지 조회 api 개발, dto 개발

---
+++ src/main/java/com/example/villagerservice/member/dto/MemberDetail.java
@@ -0,0 +1,39 @@
+package com.example.villagerservice.member.dto;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDetail {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response {
+        private Long memberId;
+        private String nickName;
+        private String email;
+        private String introduce;
+        private Integer mannerPoint;
+        private Long partyRegisterCount;
+        private Long postRegisterCount;
+        private Long follow;
+        private Long follower;
+        private boolean followState;
+        private List<String> tags = new ArrayList<>();
+
+        public Response(Long partyRegisterCount, Long postRegisterCount, Long follow, Long follower) {
+            this.partyRegisterCount = partyRegisterCount;
+            this.postRegisterCount = postRegisterCount;
+            this.follow = follow;
+            this.follower = follower;
+            this.followState = false;
+        }
+
+        public void addTags(List<String> tags) {
+            this.tags = tags;
+        }
+    }
+}
