5bab84d3333086eb45bb65c42346dbc191e40c22
donghun93 <alswn4516@gmail.com>
2022-12-17 18:34:50 +0900

    town api 컨트롤러 import 문 정리

--- src/main/java/com/example/villagerservice/town/api/TownApiController.java
+++ src/main/java/com/example/villagerservice/town/api/TownApiController.java
@@ -3,10 +3,7 @@ package com.example.villagerservice.town.api;
 import com.example.villagerservice.town.dto.TownList;
 import com.example.villagerservice.town.service.TownQueryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
