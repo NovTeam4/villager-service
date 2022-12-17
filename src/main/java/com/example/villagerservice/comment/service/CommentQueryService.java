ee1d3ca25a9b27503f6a06227747fa04c8a6730a
KyeongSeopHam <rudtjq5724@gmail.com>
2022-12-18 00:45:32 +0900

    오타 수정

--- src/main/java/com/example/villagerservice/comment/service/CommentQueryService.java
+++ src/main/java/com/example/villagerservice/comment/service/CommentQueryService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public interface CommentQueryService {
     //                      회원후기번호     회원번호           <평점,후기내용>          상대방아이디
-    void commentCreate(Long memerId, CommentDto.CommentRequest request);
+    void commentCreate(Long memberId,  CommentDto.CommentRequest request);
 
     CommentDto.findByAllResponse getCommentList();
 
