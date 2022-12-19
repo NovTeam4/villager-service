74296f99dd94074aa8b19b9f5b72467ae766919c
donghun93 <alswn4516@gmail.com>
2022-12-19 10:57:29 +0900

    rabbitmq 관련 에러 발생 수정
    - 스프링부트 시작 시 rabbitmq 관련 에러 발생함, try catch 로 수정

--- src/main/java/com/example/villagerservice/config/rabbitmq/ApplicationStartup.java
+++ src/main/java/com/example/villagerservice/config/rabbitmq/ApplicationStartup.java
@@ -1,18 +1,24 @@
 package com.example.villagerservice.config.rabbitmq;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
     private final RabbitAdmin rabbitAdmin;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        rabbitAdmin.initialize();
+        try {
+            rabbitAdmin.initialize();
+        } catch (Exception e) {
+            log.error("onApplicationEvent : ", e);
+        }
     }
 }
