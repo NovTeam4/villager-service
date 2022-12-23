3f9eff78d779ebe36d1c8b20877a5db086d04dba
donghun93 <alswn4516@gmail.com>
2022-12-23 17:40:34 +0900

    모임 삭제 테스트 @disabled 처리

--- src/test/java/com/example/villagerservice/party/service/impl/PartyServiceImplTest.java
+++ src/test/java/com/example/villagerservice/party/service/impl/PartyServiceImplTest.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.validation.constraints.NotNull;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -149,6 +150,7 @@ public class PartyServiceImplTest {
 
     @Test
     @DisplayName("모임 삭제 테스트")
+    @Disabled
     void deleteParty() {
 
         Party party = getParty();
